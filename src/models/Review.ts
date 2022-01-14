9135e5d0ee1cb1043505988813ffc9a0e3b30aeb
Ezekiel <ajayiezekiel9@gmail.com>
2022-01-14 16:29:58 +0100

    Created review model

---
+++ src/models/Review.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+interface Review {
+    description: string,
+    rating: number,
+    // product: mongoose.Types.ObjectId,
+    // user: mongoose.Types.ObjectId,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+
+const ReviewSchema = new mongoose.Schema<Review>({
+    description: {
+        type: String,
+        required: [true, 'Please add a review for the product'],
+        maxlength: [500, 'You cannot write more than 500 words']
+    },
+    rating: {
+        type: Number,
+        min: [1, 'It cannot be less than one'],
+        max: [10, 'It cannnot be more than ten']
+    },
+    // product: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Product',
+    //     required: true
+    // },
+    // user: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User',
+    //     required: true
+    // },
+}, {
+    timestamps: true
+});
+
+
+export default mongoose.model<Review>('Review', ReviewSchema);
