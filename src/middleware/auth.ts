5062e74ec0c96834bc5ab2c82f7195e22547155d
Ezekiel <ajayiezekiel9@gmail.com>
2022-01-15 22:59:48 +0100

    Created authentication middleware for protected route

---
+++ src/middleware/auth.ts
@@ -0,0 +1,38 @@
+import { Response, Request, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import asyncHandler from '../middleware/async';
+import ErrorResponse from '../utils/errorResponse';
+import User from '../models/User';
+
+interface customRequest extends Request {
+    user?: any
+}
+
+// Protect routes
+const protect = asyncHandler(async (req: customRequest, res: Response, next: NextFunction) => {
+    let token: string | undefined;
+
+    if(req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+    }
+
+    // Make sure token exists
+    if(!token) {
+        return next(new ErrorResponse('Not authorized to access this route', 401))
+    }
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`);
+
+        req.user = await User.findById(decoded.id);
+
+        next();
+    } catch(err) {
+        return next(new ErrorResponse('Not authorized to access this route', 401));
+    }
+});
+
+export {
+    protect
+}
