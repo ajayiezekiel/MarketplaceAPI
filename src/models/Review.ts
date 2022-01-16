cb6746b736dffb1659b864b5fc798b6e5cb87a17
Ezekiel <ajayiezekiel9@gmail.com>
2022-01-16 23:30:59 +0100

    Implemented getAverageRating static for Review model

--- src/models/Review.ts
+++ src/models/Review.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+import Product from './Product';
 
 interface Review {
     text: string,
     rating: number,
-    // product: mongoose.Types.ObjectId,
-    // user: mongoose.Types.ObjectId,
+    product: mongoose.Types.ObjectId,
+    user: mongoose.Types.ObjectId,
     createdAt: Date,
     updatedAt: Date
 }
@@ -21,19 +22,56 @@ const ReviewSchema = new mongoose.Schema<Review>({
         min: [1, 'It cannot be less than one'],
         max: [10, 'It cannnot be more than ten']
     },
-    // product: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Product',
-    //     required: true
-    // },
-    // user: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'User',
-    //     required: true
-    // },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
 }, {
     timestamps: true
 });
+
+// Prevent user from submitting more than one review per product
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+// Static method to get average rating and save
+ReviewSchema.statics.getAverageRating = async function(productId) {
+    const obj = await this.aggregate([
+        {
+            $match: { product: productId }
+        },
+        {
+            $group: {
+                _id: '$product',
+                averageRating: { $avg: '$rating' }
+            }
+        }
+    ]); 
+    
+    try {
+        await Product.findByIdAndUpdate(productId, {
+            averageRating: obj[0].averageRating
+        });
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+// Execute getAverageRating after save
+ReviewSchema.post('save', function() {
+    this.constructor.getAverageRating(this.product);
+});
+
+// Execute getAverageRating before remove
+ReviewSchema.pre('remove', function() {
+    this.constructor.getAverageRating(this.product);
+});
+
 
 
 export default mongoose.model<Review>('Review', ReviewSchema);
