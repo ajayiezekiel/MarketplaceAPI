aa82f561f19d1e2cd64cfdbd571aa454d57df36e
Ezekiel <ajayiezekiel9@gmail.com>
2022-01-16 23:27:52 +0100

    Refactored the getProducts implementation

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -3,89 +3,33 @@ import Product from '../models/Product';
 import ErrorResponse from '../utils/errorResponse';
 import asyncHandler from '../middleware/async';
 
-interface paginationInt {
-    page: number,
-    limit: number
+interface PaginationInt {
+    page: number;
+    limit: number;
 };
 
-interface customRequest extends Request {
-    user?: any
+interface AbstractedRes {
+    success: boolean;
+    count: number;
+    pagination?: {next?: PaginationInt, prev?: PaginationInt}
+    data: any
 };
+
+interface customResponse extends Response {
+    abstractedResults?: AbstractedRes
+};
+
+interface CustomRequest extends Request {
+    user?: any
+}
 
 
 
 // @desc    Get all products
 // @route   GET /api/v1/products
 // @access  Public
-const getProducts = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    let query;
-
-    // Copy req.query
-    const reqQuery = { ...req.query }
-
-    // Fields to exclude
-    const removedLists: string[] = ['limit', 'select', 'sort', 'page']   
-
-    // Loop over removeFields and delete them from reqQuery
-    removedLists.forEach(param => delete reqQuery[param]);
-
-    // Create query string
-    let queryStr = JSON.stringify(reqQuery);
-
-    // Create operators 
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-
-    // Finding Resource
-    query = Product.find(JSON.parse(queryStr));
-
-    // Select Fields
-    if(req.query.select){
-        const fields = (req.query.select as string).split(',').join(' ');
-        query = query.select(fields);
-    }
-
-    if (req.query.sort) {
-        const fields = (req.query.sort as string).split(',').join(' ');
-        query = query.sort(fields);
-    } else {
-        query = query.sort('-createdAt');
-    }
-
-    // Pagination
-    const page = parseInt((req.query.page as string), 10) || 1;
-    const limit = parseInt((req.query.limit as string), 10) || 10;
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const total = await Product.countDocuments();
-
-    query = query.skip(startIndex).limit(limit);
-
-    // Executing query
-    const results = await query;
-
-    const pagination: {next?: paginationInt, prev?: paginationInt} = {};
-
-    if(endIndex < total) {
-        pagination.next = {
-            page: page + 1,
-            limit
-        }
-    }
-
-    if(startIndex > 0) {
-        pagination.prev = {
-            page: page - 1,
-            limit
-        }
-    }
-    
-
-    res.status(200).json({
-        success: true,
-        count: results.length,
-        pagination,
-        data: results
-    });
+const getProducts = asyncHandler(async (req: Request, res: customResponse, next: NextFunction) => {
+    res.status(200).json(res.abstractedResults)
 });
 
 // @desc    Get single product
@@ -107,7 +51,7 @@ const getProduct = asyncHandler(async (req: Request, res: Response, next: NextFu
 // @desc    Create product
 // @route   POST /api/v1/products
 // @access  Private
-const addProduct = asyncHandler(async (req: customRequest, res: Response, next: NextFunction) => {
+const addProduct = asyncHandler(async (req: CustomRequest, res: Response, next: NextFunction) => {
     req.body.user = req.user.id
     const product = await Product.create(req.body);
 
